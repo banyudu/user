a639880846d9102fc85cc15091cf0a3745f36d7d
Yudu Ban <banyudu@innobuddy.com>
2017-09-06 15:08:23 +0800

    add routers and functions

--- handler.js
+++ handler.js
@@ -1,6 +1,42 @@
 'use strict';
 
+module.exports.createUser = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
 module.exports.getUser = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
+module.exports.updateUser = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
+
+module.exports.deleteUser = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
