6336e876169385eb0f0832d535d75615ba472e77
Yudu Ban <banyudu@innobuddy.com>
2017-09-06 16:29:24 +0800

    refactor code structure

--- handler.js
+++ handler.js
@@ -1,49 +1,40 @@
 'use strict';
 
-module.exports.createUser = (event, context, callback) => {
+const User = require('./user');
+const co = require('co');
+const UserHandler = new User();
+
+function* run(event, context, callback, handler) {
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: {
+      code: 1,
+      msg: `Unknown Error`,
+    },
   };
-
+  try {
+    const func = UserHandler[handler];
+    if (func) {
+      const data = yield func();
+      response.body = {
+        code: 0,
+        data,
+      };
+    }
+  } catch (error) {
+    console.error(error);
+  }
   callback(null, response);
-};
+}
 
-module.exports.getUser = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-};
-
-module.exports.updateUser = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-};
-
-module.exports.deleteUser = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-};
+// routers
+const attrs = Object.getOwnPropertyNames(User.prototype);
+for (const attr of attrs) {
+  const blacklist = ['constructor'];
+  const type = typeof User.prototype[attr];
+  if (type === 'function') {
+    module.exports[attr] = (event, context, callback) => {
+      co(run(event, context, callback, attr));
+    }
+  }
+}
---
+++ user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = class User {
+  constructor() {
+    // do nothing
+  }
+
+  *createUser(params) {
+    // TODO: implement this function
+  }
+
+  *getUser(params) {
+    // TODO: implement this function
+  }
+
+  *updateUser(params) {
+    // TODO: implement this function
+  }
+
+  *deleteUser(params) {
+    // TODO: implement this function
+  }
+}
